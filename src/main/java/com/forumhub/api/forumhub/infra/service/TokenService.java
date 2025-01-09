1fe5cee93119c781acaa07b4dd7824378869dd83
eduardobortolotti1 <tiolollipop@gmail.com>
2025-01-09 03:45:54 -0300

    refactor: refactored TokenService

--- src/main/java/com/forumhub/api/forumhub/infra/service/TokenService.java
+++ src/main/java/com/forumhub/api/forumhub/infra/service/TokenService.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.forumhub.api.forumhub.domain.usuario.Usuario;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,15 +21,22 @@ public class TokenService {
     //    TODO: replace secret from JWT token gen
     @Value("${api.security.token.secret}")
     private String SECRET_KEY;
+    private Algorithm algorithm;
+    private final String ISSUER = "ForumHub API";
 
-    public String gerarToken(String email) {
+    @PostConstruct
+    public void init() {
+        this.algorithm = Algorithm.HMAC256(SECRET_KEY);
+    }
+
+    public String gerarToken(String email, Long idUsuario) {
         try {
-            var algoritmo = Algorithm.HMAC256(SECRET_KEY);
             return JWT.create()
                     .withIssuer("ForumHub API")
                     .withSubject(email)
+                    .withClaim("userId", idUsuario)
                     .withExpiresAt(dataExpiracao())
-                    .sign(algoritmo);
+                    .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("erro ao gerar token jwt", exception);
         }
@@ -39,7 +47,11 @@ public class TokenService {
     }
 
     public String getSubjectFromJwtToken(String token) {
-        return JWT.require(Algorithm.HMAC256(SECRET_KEY)).withIssuer("ForumHub API").build().verify(token).getSubject();
+        return JWT.require(algorithm).withIssuer(ISSUER).build().verify(token).getSubject();
+    }
+
+    public long getUserIdFromJwtToken(String token) {
+        return JWT.require(algorithm).withIssuer(ISSUER).build().verify(token).getClaim("userId").asLong();
     }
 
     public String extractJwtFromRequest(HttpServletRequest request) {
@@ -52,7 +64,9 @@ public class TokenService {
 
     public boolean validateJwtToken(String token) {
         try {
-            JWTVerifier verifier = JWT.require(com.auth0.jwt.algorithms.Algorithm.HMAC256(SECRET_KEY))
+            JWTVerifier verifier = JWT
+                    .require(com.auth0.jwt.algorithms.Algorithm.HMAC256(SECRET_KEY))
+                    .withIssuer(ISSUER)
                     .build();
             verifier.verify(token); // This will throw an exception if the token is invalid
             return true;
