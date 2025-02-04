51b27cb330df8146270ee4f30d61e28783c9de40
eduardobortolotti1 <tiolollipop@gmail.com>
2025-02-04 18:12:11 -0300

    fix: fixed annotation validation errors

--- src/main/java/com/forumhub/api/forumhub/domain/registro/RegistroDTO.java
+++ src/main/java/com/forumhub/api/forumhub/domain/registro/RegistroDTO.java
@@ -1,23 +1,21 @@
 package com.forumhub.api.forumhub.domain.registro;
 
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import org.hibernate.validator.constraints.Range;
 
 public record RegistroDTO(
-        @Pattern(regexp = "\\S.*\\S", message = "O nome não pode começar ou terminar com espaços em branco.")
-        @Range(min = 3, max = 150, message = "O nome deve ter entre 3 e 150 caracteres!")
+        @Pattern(regexp = "^\\S(.*\\S)?$", message = "O nome não pode começar ou terminar com espaços em branco.")
+        @Size(min = 3, max = 150, message = "O nome deve ter entre 3 e 150 caracteres!")
         String nome,
 
-        @Pattern(regexp = "\\S.*\\S", message = "O email não pode começar ou terminar com espaços em branco.")
-        @Email
+        @Pattern(regexp = "^\\S(.*\\S)?$", message = "O email não pode começar ou terminar com espaços em branco.")
+        @Email(message = "O email deve ser válido!")
         @Size(max = 100, message = "O email não pode ter mais que 100 caracteres")
         String email,
 
         @Pattern(regexp = ".*[A-Z].*", message = "A senha deve conter pelo menos 1 letra maiúscula!")
-        @Range(min = 8, max = 150, message = "A senha deve ter entre 8 e 50 caracteres!")
+        @Size(min = 8, max = 50, message = "A senha deve ter entre 8 e 50 caracteres!")
         String senha
 ) {
 }
